b1609cafc972a1df447e1cba4362ce89089a02d7
ankhwyf <1422174381@qq.com>
2016-12-18 18:37:56 +0800

    可以从数据库调出数据并显示

--- app/src/main/java/com/worktogether/subwayticket/MainActivity.java
+++ app/src/main/java/com/worktogether/subwayticket/MainActivity.java
@@ -1,26 +1,37 @@
 package com.worktogether.subwayticket;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.worktogether.subwayticket.bean.OrderHistory;
+import com.worktogether.subwayticket.bean.SubwayLine;
 import com.worktogether.subwayticket.util.Constants;
 import com.worktogether.subwayticket.util.SharedPreferencesUtils;
 import com.wx.wheelview.adapter.ArrayWheelAdapter;
 import com.wx.wheelview.widget.WheelView;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
@@ -28,34 +39,76 @@ import cn.bmob.v3.listener.QueryListener;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Boolean isLogin=false;
-    Integer count=1;
+    private Boolean isLogin=false;
+    private int mCount=1;
+    private double money=0.0;
+    //设置静态表名
+    private static final String LINEONE="subway_station_1";
+    private static final String LINEFOUR="subway_station_4";
+    //设置静态字段名
+    private static final String STATIONID="objectId";
+    private static final String STATIONNAME="station_name";
+    private static final String DISTANCE="distance";
+    // 选择的类型
+    private static final int TYPE_START= 0x01;
+    private static final int TYPE_END = 0x02;
 
+    //支付按钮
     private Button mBtnPay;
+    //选择出发站点
     private TextView mTvSelectArrived;
+    //选择到达站点
     private TextView mTvSelectDepart;
+    //票的张数
     private TextView mTvTicketCount;
+    //退出登录
     private LinearLayout mLayoutExit;
+    //购票记录
     private TextView mTvOrderList;
+    //票数加加++
     private TextView mTvAdd;
+    //票数减减--
     private TextView mTvSub;
-
-    private WheelView mWheelView;
+    private TextView mTvMoney;
+    //站点滚轮
+    private WheelView mWheelStations;
+    //地铁线路滚轮
+    private WheelView mWheelLines;
+    //popupWindow 弹出框确定按钮
     private Button mBtnConfirm;
+    //popupWindow 弹出框取消按钮
     private Button mBtnCancel;
-
+    //监听起点终点是否已选择
     private MyTextWatcher mTextWatcher = new MyTextWatcher();
-
+    //popupWindow 弹出框
     private PopupWindow mPopupWindow = null;
+    //站点ArrayList
+    private List<String> mStationListOne = new ArrayList<>();
+    private List<String> mStationListFour= new ArrayList<>();
+    //地铁线路ArrayList
+    private List<String> mLineList = new ArrayList<>();
+   // 选定的站点
+    private String mSelectedStation;
+    // 选定的地铁线路
+    private int mDepartSelectedLine=0;
+    private int mArriveSelectedLine=0;
+    private int mDepartSelectedStation=0;
+    private int mArriveSelectedStation=0;
+    private int mCurSelectedLine=0;
+    private int mCurSelectedStation=0;
 
+    private int mSelectedType = TYPE_START;
+    private List<SubwayLine> mSubwayLineOneList=new ArrayList<>() ;
+    private List<SubwayLine> mSubwayLineFourList=new ArrayList<>() ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        queryLine(LINEONE,mSubwayLineOneList,mStationListOne);
+        queryLine(LINEFOUR,mSubwayLineFourList,mStationListFour);
         // 关联控件
         findViews();
-        //监听始终点是否已选择
+        //监听事件
         initListener();
 
     }
@@ -69,47 +122,96 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mTvOrderList = (TextView) findViewById(R.id.order_list);
         mTvAdd = (TextView) findViewById(R.id.add_btn);
         mTvSub = (TextView) findViewById(R.id.sub_btn);
+        mTvMoney=(TextView)findViewById(R.id.should_pay);
     }
 
     private void initListener() {
+        //监听始终点是否已全部选择
         mTvSelectArrived.addTextChangedListener(mTextWatcher);
         mTvSelectDepart.addTextChangedListener(mTextWatcher);
         mLayoutExit.setOnClickListener(this);
         mTvOrderList.setOnClickListener(this);
         mTvAdd.setOnClickListener(this);
         mTvSub.setOnClickListener(this);
+        mTvSelectArrived.setOnClickListener(this);
+        mTvSelectDepart.setOnClickListener(this);
     }
 
     /**
      * 显示选择站点的popup windows
      */
     private void showPopupWindow() {
+        //若为空，则将mPopupWindow实例化
         if (mPopupWindow == null) {
+            //显示popup_select_stations_layout布局
             View popupView = LayoutInflater.from(this).inflate(R.layout.popup_select_stations_layout, null);
-            PopupWindow window = new PopupWindow(popupView,
+            mPopupWindow = new PopupWindow(popupView,
                     LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, true);
             mPopupWindow.setTouchable(true);
             // 点击空白区域，窗口消失
             mPopupWindow.setOutsideTouchable(true);
 
-            mWheelView = (WheelView) popupView.findViewById(R.id.wv_stations);
             mBtnConfirm = (Button) popupView.findViewById(R.id.btn_confirm);
             mBtnCancel = (Button) popupView.findViewById(R.id.btn_cancel);
 
             // 初始化WheelView
-            initWheelView();
+            initWheelView(popupView);
 
             mBtnConfirm.setOnClickListener(this);
             mBtnCancel.setOnClickListener(this);
-//            mWheelView.set
         }
 
+        mPopupWindow.showAtLocation(MainActivity.this.findViewById(R.id.activity_main), Gravity.BOTTOM, 0, 0);
     }
 
-    private void initWheelView() {
+    /**
+     * 初始化滚轮内容
+     */
+    private void initWheelView(View view) {
 
-        if (mWheelView != null) {
+        //ArrayList
+        if (mLineList.size() == 0) {
+            mLineList.add("地铁一号线");
+            mLineList.add("地铁四号线");
         }
+        //站点Wheel
+        if (mWheelStations == null) {
+            mWheelStations = (WheelView) view.findViewById(R.id.wv_stations);
+            mWheelStations.setWheelAdapter(new ArrayWheelAdapter(this));
+            //设置滚轮主题
+            mWheelStations.setSkin(WheelView.Skin.Holo);
+            mWheelStations.setWheelData(mStationListOne);
+            mWheelStations.setOnWheelItemSelectedListener(new WheelView.OnWheelItemSelectedListener() {
+                @Override
+                public void onItemSelected(int position, Object o) {
+                    mSelectedStation = (String) o;
+                    mCurSelectedStation=position;
+                }
+            });
+        }
+        //地铁线路Wheel
+        if (mWheelLines == null) {
+            mWheelLines = (WheelView) view.findViewById(R.id.wv_lines);
+            mWheelLines.setWheelAdapter(new ArrayWheelAdapter(this));
+            //设置滚轮主题
+            mWheelLines.setSkin(WheelView.Skin.Holo);
+            //载入地铁线路数据ArrayList
+            mWheelLines.setWheelData(mLineList);
+
+            mWheelLines.setOnWheelItemSelectedListener(new WheelView.OnWheelItemSelectedListener() {
+                @Override
+                public void onItemSelected(int position, Object o) {
+                    mCurSelectedLine=position;
+                    if(position==0){
+                        mWheelStations.setWheelData(mStationListOne);
+                    }
+                    else {
+                        mWheelStations.setWheelData(mStationListFour);
+                    }
+                }
+            });
+        }
+
     }
 
     @Override
@@ -136,27 +238,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             //出发站
             case R.id.select_depart:
-
-
+                mSelectedType = TYPE_START;
+                showPopupWindow();
                 break;
             //到达站
             case R.id.select_arrived:
-
+                mSelectedType = TYPE_END;
+                showPopupWindow();
                 break;
             //增加票数
             case R.id.add_btn:
-                if(count<6){
-                    count++;
-                    mTvTicketCount.setText(String.valueOf(count));
+                if(mCount<6){
+                    mCount++;
+                    mTvTicketCount.setText(String.valueOf(mCount));
+                    mTvMoney.setText("￥"+(money*mCount));
                 }
                 break;
             //减少票数
             case R.id.sub_btn:
-                if(count!=1){
-                    count--;
-                    mTvTicketCount.setText(String.valueOf(count));
+                if(mCount!=1){
+                    mCount--;
+                    mTvTicketCount.setText(String.valueOf(mCount));
+                    mTvMoney.setText("￥"+(money*mCount));
                 }
-
                 break;
             //支付按钮
             case R.id.pay_btn:
@@ -172,10 +276,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case R.id.btn_confirm:
+                mPopupWindow.dismiss();
+                //出发站点
+                if (mSelectedType == TYPE_START) {
+                    mTvSelectDepart.setText( mSelectedStation);
+                    mTvSelectDepart.setTextColor(Color.BLACK);
+                    // 选定出发站点的地铁线路和站点名称
+                    mDepartSelectedLine=mCurSelectedLine;
+                    mDepartSelectedStation=mCurSelectedStation;
+                }
+                //到达站点
+                else {
+                    mTvSelectArrived.setText(mSelectedStation);
+                    mTvSelectArrived.setTextColor(Color.BLACK);
+                    // 选定到达站点的地铁线路和站点名称
+                    mArriveSelectedLine=mCurSelectedLine;
+                    mArriveSelectedStation=mCurSelectedStation;
+                }
 
                 break;
             case R.id.btn_cancel:
-
+                mPopupWindow.dismiss();
                 break;
         }
     }
@@ -191,27 +312,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String depart = mTvSelectDepart.getText().toString().trim();
             if (TextUtils.isEmpty(arrived) || TextUtils.isEmpty(depart)) {
                 mBtnPay.setEnabled(false);
-            } else {
+            }
+            else if(arrived.equals(depart)){
+                mBtnPay.setEnabled(false);
+                Toast.makeText(getApplicationContext(),"站点不能相同！",Toast.LENGTH_SHORT).show();
+            }
+            else {
                 mBtnPay.setEnabled(true);
+                money=calTicketPrice(mDepartSelectedLine,mArriveSelectedLine,mDepartSelectedStation,mArriveSelectedStation);
+                mTvMoney.setText("￥"+(money*mCount));
+                int color = ContextCompat.getColor(MainActivity.this, R.color.colorLightRed);
+                mTvMoney.setTextColor(color);
             }
         }
 
         @Override
         public void afterTextChanged(Editable s) {
-
         }
     }
 
     /**
+     * 地铁1号线
      * 查询结果是JSONArray ，需自行解析
+     * [{"updatedAt":"2016-12-16 11:26:04",
+     *   "station_name":"湘湖",
+     *   "distance":0,
+     *   "objectId":"Vo8a777K",
+     *   "createdAt":"2016-12-16 11:25:27"},
      * **/
-    public void queryData(){
-        BmobQuery query=new BmobQuery("subway_station_1");
+    //传入表名 和 用于存储地铁具体站点信息的List
+    public void queryLine(final String lineStr, final List<SubwayLine> mSubwayLineList, final List<String> mStationList){
+        BmobQuery query=new BmobQuery(lineStr);
         query.findObjectsByTable(new QueryListener<JSONArray>() {
             @Override
             public void done(JSONArray jsonArray, BmobException e) {
                 if(e==null){
-                    Log.d("bmob","查询成功："+jsonArray.toString());
+                    //遍历
+                    for(int i=0;i<jsonArray.length();i++){
+                        try {
+                            JSONObject obj=jsonArray.getJSONObject(i);
+                            String id=obj.getString(STATIONID);
+                            String stationName=obj.getString(STATIONNAME);
+                            Double distance=obj.getDouble(DISTANCE);
+
+                            SubwayLine stationInfo=new  SubwayLine(lineStr);
+                            stationInfo.setDistance(distance);
+                            stationInfo.setStation_name(stationName);
+                            mSubwayLineList.add(stationInfo);
+                            mStationList.add(stationName);
+
+                        } catch (JSONException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
                 } else {
                     Log.d("bmob","查询失败："+e.getMessage()+","+e.getErrorCode());
                 }
@@ -219,4 +372,77 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+    //计算票价
+    public double calTicketPrice(int departLine,int arriveLine,int departStation,int arriveStation){
+        double price=0.0,distance=0.0;
+        //情况一: 选择同一条线路
+        if(departLine==arriveLine){
+            Log.d("bmob","departStation："+mDepartSelectedStation);
+            Log.d("bmob","arriveStation："+mArriveSelectedStation);
+            //交换起点和终点
+            if(arriveStation<departStation){
+                swap(departStation,arriveStation);
+            }
+            //若两个站点为这三个站点中（近江，火车东站，彭埠）的任意两个的情况
+            //则选取四号线的票价
+            //一号线的两个站点
+            if(arriveLine==0){
+                String departName=mSubwayLineOneList.get(departStation).getStation_name();
+                String arriveName=mSubwayLineOneList.get(arriveStation).getStation_name();
+                if(departName.equals("近江")||departName.equals("火车东站")){
+                    if(arriveName.equals("火车东站")||arriveName.equals("彭埠")){
+                        arriveLine=1;
+                    }
+                }
+            }
+            switch (arriveLine){
+                //地铁一号线
+                case 0:
+                    for(int i=arriveStation;i>departStation;i--) {
+                        distance += mSubwayLineOneList.get(i).getDistance();
+                        Log.d("bmob","i："+i+"，" + "distance："+distance);
+                    }
+                    break;
+               //地铁四号线
+                case 1:
+                    for(int i=arriveStation;i>=departStation;i--) {
+                        distance+=mSubwayLineFourList.get(i).getDistance();
+                    }
+                    break;
+            }
+        }
+        //情况二: 选择不同线路的站点
+        else {
+            //若两个站点为这三个站点中（近江，火车东站，彭埠）的任意两个的情况
+            //则选取四号线的票价
+            String departName=mTvSelectDepart.getText().toString();
+            String arriveName= mTvSelectArrived.getText().toString();
+
+            /*
+            * 站点之间跨度大：包含近江和火车东站
+            * **/
+        }
+        price=calculatePrice(distance);
+        return price;
+    }
+
+    public void swap(Integer a,Integer b){
+        Integer temp;
+        temp=a;
+        a=b;
+        b=temp;
+    }
+
+    public double calculatePrice(double distance){
+        int temp=(int)(distance/ 1000 + 0.5);
+        double price=0.0;
+        if(temp<=4) price=2.0;
+        else if(temp<=8) price=3.0;
+        else if(temp<=12) price=4.0;
+        else if(temp<=18) price=5.0;
+        else if(temp<=24) price=6.0;
+        else if(temp<=32) price=7.0;
+        else if(temp<=40) price=8.0;
+        return price;
+    }
 }
--- app/src/main/java/com/worktogether/subwayticket/bean/SubwayLine.java
+++ app/src/main/java/com/worktogether/subwayticket/bean/SubwayLine.java
@@ -6,15 +6,13 @@ import cn.bmob.v3.BmobObject;
  * Created by Ankhwyf on 16/12/17.
  */
 
-
-public class SubwayLine2 extends BmobObject {
+public class SubwayLine extends BmobObject {
 
     private String station_name;
-    private Float distance;
-
+    private Double distance;
     //对应数据库中的表名
-    public SubwayLine2(){
-        this.setTableName("subway_station_2");
+    public SubwayLine(String tableName){
+        this.setTableName(tableName);
     }
 
     public String getStation_name() {
@@ -25,11 +23,11 @@ public class SubwayLine2 extends BmobObject {
         this.station_name = station_name;
     }
 
-    public Float getDistance() {
+    public Double getDistance() {
         return distance;
     }
 
-    public void setDistance(Float distance) {
+    public void setDistance(Double distance) {
         this.distance = distance;
     }
 }
