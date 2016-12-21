d7dbbe727e4b1a3b60d61ccbe8471cf50e641c6f
unknown <>
2016-12-21 20:08:34 +0800

    删除部分xml文件

--- app/src/main/java/com/worktogether/subwayticket/PayDetailActivity.java
+++ app/src/main/java/com/worktogether/subwayticket/PayDetailActivity.java
@@ -2,12 +2,22 @@ package com.worktogether.subwayticket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class PayDetailActivity extends AppCompatActivity {
+
+    private TextView QRCodeNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_detail);
+
+        QRCodeNum = (TextView) findViewById(R.id.QRCodeStr);
+    }
+
+    public void makeQRCode(View view){
+
     }
 }
